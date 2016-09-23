342285865b4f8599ea2dd1915e85547a60a3b904
Rowan Parkinson <Rowan.Parkinson@myob.com>
2016-09-23 16:12:06 +1000

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,52 @@ var gulp        = require('gulp');
 var prefix      = require('gulp-autoprefixer');
 var cssnano     = require('gulp-cssnano');
 var concat      = require('gulp-concat');
+var minifyCSS   = require('gulp-minify-css');
+var rename      = require('gulp-rename');
 var sass        = require('gulp-sass');
+var sassLint    = require('gulp-sass-lint');
 var uglify      = require('gulp-uglify');
 var browserSync = require('browser-sync');
 var cp          = require('child_process');
 var runSequence = require('run-sequence');
 
-var messages = {
-  jekyllDev: 'Running: $ jekyll build for dev',
-  jekyllProd: 'Running: $ jekyll build for prod'
-};
+gulp.task('fonts', function() {
+  return gulp.src(['./frontend/_fonts/**/*'])
+  .pipe(gulp.dest('./dist/assets/fonts/'));
+});
+
+gulp.task('javascript:bootstrap', function () {
+  return gulp.src(['./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'])
+    .pipe(rename('bootstrap-3.3.7.js'))
+    .pipe(gulp.dest('./frontend/_js/etc/designs/myob/footlibs/js/vendor'))
+});
+
+gulp.task('javascript:jquery', function () {
+  return gulp.src(['./node_modules/jquery/dist/jquery.js'])
+    .pipe(rename('jquery-2.2.4.js'))
+    .pipe(gulp.dest('./frontend/_js/etc/designs/myob/footlibs/js/vendor'))
+});
+
+gulp.task('javascript:jquery-lazyload', function () {
+  return gulp.src(['./node_modules/jquery-lazyload/jquery.lazyload.js'])
+    .pipe(rename('jquery.lazyload-1.9.7.js'))
+    .pipe(gulp.dest('./frontend/_js/etc/designs/myob/footlibs/js/vendor'))
+});
+
+gulp.task('javascript', function() {
+  return gulp.src(['./frontend/_js/**/*.js'])
+  .pipe(concat('scripts.js'))
+  .pipe(uglify())
+  .pipe(browserSync.reload({stream:true}))
+  .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('scripts', [
+  'javascript:bootstrap',
+  'javascript:jquery',
+  'javascript:jquery-lazyload',
+  'javascript'
+]);
 
 gulp.task('sass', function () {
   return gulp.src(['./frontend/_sass/**/*.scss'])
@@ -21,29 +57,28 @@ gulp.task('sass', function () {
   }))
   .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
   .pipe(browserSync.reload({stream:true}))
-  .pipe(gulp.dest('./aem/css'));
+  .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src(['./frontend/_js/**/*.js'])
-  .pipe(concat('scripts.js'))
-  .pipe(browserSync.reload({stream:true}))
-  .pipe(gulp.dest('./aem/js'));
+gulp.task('sass-lint', function () {
+  return gulp.src(['./frontend/_sass/**/*.s+(a|c)ss'])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
 });
 
-gulp.task('jekyll-dev', function (done) {
-  browserSync.notify(messages.jekyllDev);
+gulp.task('jekyll', function (done) {
   return cp.spawn('jekyll', ['build', '--config', './config/_config.yml'], {stdio: 'inherit'})
  .on('close', done);
 });
 
-gulp.task('jekyll-rebuild', ['jekyll-dev'], function () {
+gulp.task('jekyll-rebuild', ['jekyll'], function () {
   browserSync.reload();
 });
 
 gulp.task('browser-sync', function() {
   browserSync.init({
-    server: "aem",
+    server: "dist",
     port: 1234
   });
 });
@@ -56,8 +91,11 @@ gulp.task('watch', function () {
 
 gulp.task('default', function(done) {
   return runSequence(
-    'jekyll-dev',
+    'jekyll',
+    // 'fonts',
+    // 'icons',
     'sass',
+    // 'sass-lint',
     'scripts',
     'browser-sync',
     'watch',
